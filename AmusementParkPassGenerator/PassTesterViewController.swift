f7496916a7d1751bfa9b67792e3afbb279828dfe
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-10 14:43:30 -0500

    Implement second view controller

---
+++ AmusementParkPassGenerator/PassTesterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PassTesterViewController.swift
+//  AmusementParkPassGenerator
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-10.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class PassTesterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
