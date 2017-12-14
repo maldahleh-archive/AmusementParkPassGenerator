e957a22cde9d570fd61e492e71b91d3f28fb710c
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-14 02:11:55 -0500

    Show pass information

--- AmusementParkPassGenerator/ViewControllers/PassTesterViewController.swift
+++ AmusementParkPassGenerator/ViewControllers/PassTesterViewController.swift
@@ -11,7 +11,51 @@ import UIKit
 class PassTesterViewController: UIViewController {
     var entrant: Entrant?
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let entrant = entrant {
+            if let seasonGuest = entrant as? SeasonGuest {
+                nameLabel.text = seasonGuest.name.fullName
+                typeLabel.text = "Season Pass Guest Pass"
+                return
+            }
+            
+            if let seniorGuest = entrant as? SeniorGuest {
+                nameLabel.text = seniorGuest.name.fullName
+                typeLabel.text = "Senior Guest Pass"
+                return
+            }
+            
+            if entrant is ClassicGuest {
+                nameLabel.text = "Unnamed"
+                typeLabel.text = "Guest Pass"
+            }
+            
+            if let hourlyEmployee = entrant as? HourlyEmployee {
+                nameLabel.text = hourlyEmployee.name.fullName
+                typeLabel.text = "Hourly Employee Pass"
+            }
+            
+            if let contractEmployee = entrant as? ContractEmployee {
+                nameLabel.text = contractEmployee.name.fullName
+                typeLabel.text = "Contract Employee Pass"
+            }
+            
+            if let manager = entrant as? Manager {
+                nameLabel.text = manager.name.fullName
+                typeLabel.text = "Manager Pass"
+            }
+            
+            if let vendor = entrant as? Vendor {
+                nameLabel.text = vendor.name.fullName
+                typeLabel.text = "Vendor Pass"
+            }
+        }
     }
 }
